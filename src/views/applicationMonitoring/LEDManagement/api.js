8cc6ed64a00c637fc62643d1ce75d226ca0343c3
fengcuiyu <1522092850>
2024-11-12 11:40:28 +0800

    fix: 新增LED管理相关页面以及接口联调

---
+++ src/views/applicationMonitoring/LEDManagement/api.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+//获取列表数据
+export function ledList(data) {
+  return request({
+    url: '/led/device/list',
+    method: 'post',
+    data
+  })
+}
+//保存
+export function ledSave(data) {
+    return request({
+      url: '/led/device/save',
+      method: 'post',
+      data
+    })
+}
+//删除
+export function ledDelete(data) {
+    return request({
+      url: '/led/device/delete',
+      method: 'post',
+      data
+    })
+  }
+//获取详情
+export function ledInfo(data) {
+    return request({
+      url: '/led/device/info',
+      method: 'post',
+      data
+    })
+  }
+//获取有效摄像头
+export function listData2(data) {
+    return request({
+      url: '/camera/listData2',
+      method: 'post',
+      data
+    })
+  }
---
+++ src/views/applicationMonitoring/LEDManagement/components/addInfo.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <el-dialog
+            :close-on-click-modal="false"
+            title="新增音柱"
+            :visible.sync="dialogVisible"
+            width="500px"
+            @close="closed"
+        >
+            <el-form ref="form" :model="params" :rules="rules" label-width="120px">
+                <el-form-item label="led名称" prop="name">
+                    <el-input v-model="params.name"></el-input>
+                </el-form-item>
+                <el-form-item label="ip和端口号" prop="ipPort">
+                    <el-input v-model="params.ipPort"></el-input>
+                </el-form-item>
+                <el-form-item label="查询时间" prop="zeroDay">
+                    <el-input-number v-model="params.zeroDay" :controls="false" :precision="0" class="input-num"></el-input-number>
+                    <div style="font-size: 12px;color: #909399;">查询全部数据填0</div>
+                </el-form-item>
+                <el-form-item label="摄像头">
+                    <el-select v-model="params.cameraIds" multiple placeholder="请选择"  style="width: 100%;">
+                        <el-option
+                        v-for="item in options"
+                        :key="item.id"
+                        :label="item.name"
+                        :value="item.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="closed">取消</el-button>
+                <el-button type="primary" @click="saveData" :loading="btnLoading">保存</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+<script>
+import { ledInfo,ledSave,listData2 } from "../api";
+export default {
+    props:{
+        currentId:{
+            type:String,
+            default:''
+        }
+    },
+    data(){
+        return{
+            dialogVisible:true,
+            options:[],
+            params:{
+                name:'',
+                ipPort:'',
+                zeroDay:'',
+                cameraIds:[],
+            },
+            rules:{
+                name: [{ required: true, message: "请输入led名称", trigger: "blur" }],
+                ipPort: [{ required: true, message: "请输入ip和端口号", trigger: "blur" }],
+                zeroDay: [{ required: true, message: "请输入查询时间", trigger: "blur" }],
+                cameraIds: [{ required: true, message: "请选择摄像头", trigger: "change" }],
+            },
+            btnLoading:false,
+        }
+    },
+    created(){
+        this.getlistData();
+        if(this.currentId){
+            this.getDetail();
+        }
+    },
+    methods:{
+        async getlistData(){
+            const res = await listData2();
+            this.options = res.data;
+        },
+        async getDetail(){
+            let formData = new FormData();
+            formData.append("id", this.currentId);
+            const res = await ledInfo(formData)
+            this.params = res.data;
+        },
+        closed(){
+            this.$emit('closeAdd')
+        },
+        // 保存
+        saveData(){
+            this.btnLoading = true;
+            this.$refs.form.validate((valid) => {
+                if (valid) {
+                    ledSave(this.params).then(res=>{
+                        this.$message.success('保存成功');
+                        this.btnLoading = false;
+                        this.$emit('closeAdd');
+                    }).catch(()=>{
+                        this.btnLoading = false;
+                    })
+                } else {
+                    this.btnLoading = false;
+                    return false;
+                }
+            });
+        }
+    }
+}
+</script>
+<style lang="scss">
+.input-num{
+  width: 100%;
+  .el-input__inner{
+    text-align: left !important;
+  }
+}
+</style>
